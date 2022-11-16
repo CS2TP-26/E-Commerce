a577fa9a473c7b8ae86eddb08c3f0b7914b6f196
Drkongy <80266694+Drkongy@users.noreply.github.com>
2022-11-16 09:38:40 +0000

    css link

--- stockade/stockade.php
+++ stockade/stockade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/NavBar.css">
+    <link rel="icon" href="assets/Icon.png">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/stockade.css">
+    <link rel="stylesheet" href="css/NavBar.css">
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link rel="icon" href="assets/Icon.png">
     <!-- bumua -->
-    <link rel="stylesheet" href="./Bulma/bulma.min.css">
-    <link href='./BoxIcons/css/boxicons.min.css' rel='stylesheet'>
+
     <link href='./css/main.css' rel='stylesheet'>
-    <link href='./css/stockade.css' rel='stylesheet'>
     
 
 
