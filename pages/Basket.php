9769e6436e12da860ea70caaaa9c787758782a4a
Drkongy <80266694+Drkongy@users.noreply.github.com>
2022-11-16 23:42:53 +0000

    asdsad

--- pages/Basket.php
+++ pages/Basket.php
@@ -96,5 +96,5 @@ $total_price += ($product["price"]*$product["quantity"]);
 <div style="clear:both;"></div>
 
 <div class="message_box" style="margin:10px 0px;">
-<?php echo $status; ?>
-</div>
+<?php echo $status . 'Test';  "?>
+</div> 
