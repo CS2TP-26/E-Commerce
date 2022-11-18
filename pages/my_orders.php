25d0c07445d0b507f3eb8f1e3a49e0ce9f2da6f6
Drkongy <80266694+Drkongy@users.noreply.github.com>
2022-11-18 01:27:57 +0000

    test 5

--- pages/my_orders.php
+++ pages/my_orders.php
@@ -65,14 +65,15 @@
                             $product_price = $row2['cost'];
                             $status = $row2['status'];
 
-                            echo "ID: " . $product_id . "<br>";
-                            echo "Product Name: " . $product_name . "<br>";
-                            echo "Product Image: " . $product_image . "<br>";
-                            echo "Product Description: " . $product_description . "<br>";
-                            echo "Product Price: " . $product_price . "<br>";
-                            echo "Order Status: " . $status . "<br>";
 
-                        } 
+
+                        }
+                        echo "ID: " . $product_id . "<br>";
+                        echo "Product Name: " . $product_name . "<br>";
+                        echo "Product Image: " . $product_image . "<br>";
+                        echo "Product Description: " . $product_description . "<br>";
+                        echo "Product Price: " . $product_price . "<br>";
+                        echo "Order Status: " . $status . "<br>";
                     }
 
 
