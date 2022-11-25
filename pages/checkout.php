a831464a5493916c2af14d5c2b5254735488f476
Jacques22FC <JacWinmillFC@gmail.com>
2022-11-25 23:55:36 +0000

    This is my rethik of checkout. Its not completed

--- pages/checkout.php
+++ pages/checkout.php
@@ -1,57 +1,19 @@
 <?php
 
-require '../connection.php';
-    $URL = "";
-    $KPL = "";
-if (isset($_POST['submitted'])){
+session_start();
+require_once '../connection.php';
+$db = connect();
 
-    $email = isset($_POST['email'])?addslashes($_POST['email']):false;
-    if(!preg_match("^/[\w\-]+@[\w\-]+.[\w\-]+$/^", $email))
-    {
-        $Error = "Please input a valid email";
-    }
 
-    $name=  isset($_POST['name'])?addslashes($_POST['name']):false;
-    if (!($name)){
-        echo "name wrong!";
-        exit;
-    }
 
-    $password=  isset($_POST['password'])?addslashes(password_hash($_POST['password'],PASSWORD_DEFAULT)):false;
-    if (!($password)){
-        echo "password wrong!";
-    }
+if (isset($_POST['Checkout'])) {
+  $sql = "INSERT INTO products (user_ID, product_ID) VALUES ('$_SESSION['id'] = $id;', '$_SESSION["basket"]')";
+   $result = mysqli_query($db, $sql);
 
-    $KPL = isset($_POST['Key'])?addslashes($_POST['Key']):false;
- 
-    $education =  isset($_POST['education'])?addslashes($_POST['education']):false;
-    if (!($education)){
-        echo "education wrong!";
-        exit;
-    }
-    
-    $URL = isset($_POST['URL'])?addslashes($_POST['URL']):false;
-   
-    
-    $profile = isset($_POST['profile'])?addslashes($_POST['profile']):false;
-
-    try{
-
-         
-        $stat=$db->prepare("insert into cvs values(default,?,?,?,?,?,?,?)");
-        $stat->execute(array($name,$email, $password, $KPL, $education, $URL, $profile));
-        
-        $id=$db->lastInsertId();
-        echo "Congratulations! You are now registered. Your ID is: $order_id  ";  	
-        
-     }
-     catch (PDOexception $ex){
-        echo "Sorry, a database error occurred! <br>";
-        echo "Error details: <em>". $ex->getMessage()."</em>";
-     }
-    
-     
 }
+
+     
+
 ?>
 
 
@@ -63,7 +25,11 @@ if (isset($_POST['submitted'])){
   <title>Checkout</title>
 </head>
 <body>
-  <h2>Details</h2>
+  <h2>Contents of basket</h2>
+  <div>
+     echo $_SESSION["basket"];
+  </div>
+  <h3>Details</h3>
   <form method = "post" action="checkout.php">
   <div>Details</div>
     
@@ -74,11 +40,11 @@ if (isset($_POST['submitted'])){
     <label>Security number</label>
     <input id = "textbox" type="text" name="Security number" required><br><br>
   
-	<input type="submit" value="Register" /> 
-	<input type="reset" value="clear"/>
+	<input type="Checkout" value="Checkout" /> 
+	<input type="clear" value="clear"/>
 	<input type="hidden" name="submitted" value="true"/>
   </form>  
-  <p> Checkout<a href="Login.php">Log in</a>  </p>
+  <p> Checkout<a href="my_orders.php">Checkout</a>  </p>
 
 </body>
 </html>
