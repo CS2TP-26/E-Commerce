81a9ba2551438687cc5422ff0fdc88bddba28eec
Drkongy <kongking7861@gmail.com>
2022-11-28 22:45:44 +0000

    checkout fixed

--- pages/checkout.php
+++ pages/checkout.php
@@ -3,6 +3,8 @@
 session_start();
 require_once '../connection.php';
 $db = connect();
+$product_id = $basket[$i]['id'];
+echo "product id" . $product_id;
 
 // if user is not logged in they will be redirected to the login page
 if (!isset($_SESSION['id'])) {
@@ -19,14 +21,18 @@ if (isset($_POST['checkout'])) {
     if ($basket_length = 0){
         echo "Your basket is empty";
     } else {
-      for ($i = 0; $i < $basket_length; $i++) {
-        $product_id = $basket[$i]['id'];
+			foreach ($_SESSION["basket"] as $product) {
+        $product_id = $product['id'];
         $sql = "INSERT INTO `orders` (`user_ID`, `product_ID`, `status`) VALUES ('$user_id', '$product_id', '$status')";
         $result = $db->query($sql);
-        echo "Order placed";
+        echo "result: " . $result;
+
     }
     unset($_SESSION['basket']);
-    // header('Location: orders.php');
+    echo "result: " . $result;
+
+
+    // header('Location: my_orders.php'); 
     }
 
 
@@ -48,11 +54,59 @@ if (isset($_POST['checkout'])) {
 </head>
 <body>
   <h2>Contents of basket</h2>
-  <div>
-    <?php
-     echo $_SESSION["basket"];
-    ?>
-  </div>
+  <div class="basket">
+	<?php
+
+	if (isset($_SESSION["basket"])) {
+		$total_price = 0;
+	?>
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>Image</td>
+					<td>Name </td>
+					<td>Price (£) </td>
+					<td>Total</td>
+				</tr>
+				<?php
+				foreach ($_SESSION["basket"] as $product) {
+				?>
+					<td>
+						<img src='<?php echo $product["image"]; ?>' width="50" height="40" />
+					</td>
+					<td><?php echo $product["name"]; ?><br />
+						<form method='post' action=''>
+							<input type='hidden' name='id' value="<?php echo $product["id"]; ?>" />
+							<input type='hidden' name='action' value="remove" />
+							<button type='submit' class='remove'>Remove Item</button>
+						</form>
+					</td>
+					<td>
+						<form method='post' action=''>
+							<input type='hidden' name='id' value="<?php echo $product["id"]; ?>" />
+							<input type='hidden' name='action' value="change" />
+						</form>
+					</td>
+					<td><?php echo "£" . $product["price"]; ?></td>
+					<td><?php echo "£" . $product["price"] * $product["quantity"]; ?></td>
+					</tr>
+				<?php
+					$total_price += ($product["price"] * $product["quantity"]);
+				}
+				?>
+				<tr>
+					<td colspan="5" align="right">
+						<strong>TOTAL: <?php echo "£" . $total_price; ?></strong>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	<?php
+	} else {
+		echo "<h3>Your basket is empty!</h3>";
+	}
+	?>
+</div>
   <h3>Details</h3>
   <form method = "post" action="checkout.php">
   <div>Details</div>
@@ -63,12 +117,12 @@ if (isset($_POST['checkout'])) {
     <input id = "textbox" type="card" name="card number" required><br><br>
     <label>Security number</label>
     <input id = "textbox" type="text" name="Security number" required><br><br>
-  
-	<input type="Checkout" value="Checkout" /> 
-	<input type="clear" value="clear"/>
+    <!-- submit button -->
+    <button type="submit" name="checkout">Checkout</button>
+
 	<input type="hidden" name="submitted" value="true"/>
   </form>  
-  <p> Checkout<a href="my_orders.php">Checkout</a>  </p>
+  <!-- <p> Checkout<a href="my_orders.php">Checkout</a>  </p> -->
 
 </body>
 </html>
