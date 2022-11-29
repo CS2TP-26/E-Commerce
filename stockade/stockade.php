a472d0e8a5a54e65e0fff81901e55f40a9294fe2
Drkongy <80266694+Drkongy@users.noreply.github.com>
2022-11-29 00:53:10 +0000

    fixed issues

--- stockade/stockade.php
+++ stockade/stockade.php
@@ -412,20 +412,23 @@
 
                                     // call the image upload function
                                     $img_url = uploadImage($image);
-                                    $sql = "INSERT INTO `products` (`name`, `description`, `image`, `price`, `stock`) VALUES ('$name', '$description', '$img_url', '$cost', '$stock')";
-                                    $result = $db->query($sql);
-                                    if ($result) {
+
+                                    if ($uploadOk == 1) {
+                                        $sql = "INSERT INTO `products` (`name`, `description`, `image`, `price`, `stock`) VALUES ('$name', '$description', '$img_url', '$cost', '$stock')";
+                                        $result = $db->query($sql);
+                                        if ($result) {
                                 ?>
-                                        <div class="alert alert-success" role="alert">
-                                            Product Added!
-                                        </div>
-                                    <?php
-                                    } else {
-                                    ?>
-                                        <div class="alert alert-danger" role="alert">
-                                            Error! Product Not Added!
-                                        </div>
+                                            <div class="alert alert-success" role="alert">
+                                                Product Added!
+                                            </div>
+                                        <?php
+                                        } else {
+                                        ?>
+                                            <div class="alert alert-danger" role="alert">
+                                                Error! Product Not Added!
+                                            </div>
                             <?php
+                                        }
                                     }
                                 }
                             } else {
@@ -454,57 +457,90 @@
 
 
 <?php
-    // create an image upload function
-    function uploadImage($image)
-    {
-        $target_dir = "Assets/Watches/";
-        $target_file = $target_dir . basename($image["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        // Check if image file is a actual image or fake image
-        if (isset($_POST["submit"])) {
-            $check = getimagesize($image["tmp_name"]);
-            if ($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
-                $uploadOk = 1;
-            } else {
-                echo "File is not an image.";
-                $uploadOk = 0;
-            }
-        }
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            echo "Sorry, file already exists.";
-            $uploadOk = 0;
-        }
-        // Check file size
-        if ($image["size"] > 500000000) {
-            echo "Sorry, your file is too large.";
-            $uploadOk = 0;
-        }
-        // Allow certain file formats
-        if (
-            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif"
-        ) {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-            $uploadOk = 0;
-        }
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-            // if everything is ok, try to upload file
+// create an image upload function
+function uploadImage($image)
+{
+    $target_dir = "Assets/Watches/";
+    $target_file = $target_dir . basename($image["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if (isset($_POST["submit"])) {
+        $check = getimagesize($image["tmp_name"]);
+        if ($check !== false) {
+            ?>
+            <div class="alert alert-success" role="alert">
+                <?php echo "File is an image - " . $check["mime"] . "."; ?>
+            </div>
+            <?php
+            
+            $uploadOk = 1;
         } else {
-            if (move_uploaded_file($image["tmp_name"], $target_file)) {
-                echo "The file " . htmlspecialchars(basename($image["name"])) . " has been uploaded.";
-                // get full image url
-                $img_url = "http://20.254.55.178/Assets/Watches/" . basename($image["name"]);
-                return $img_url;
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
+            ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo "File is not an image."; ?>
+            </div>
+            <?php
+            $uploadOk = 0;
         }
     }
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo "Sorry, file already exists."; ?>
+            </div>
+        <?php
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($image["size"] > 500000000) {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo "Sorry, your file is too large."; ?>
+            </div>
+        <?php
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if (
+        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif"
+    ) {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed."; ?>
+            </div>
+        <?php
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo "Sorry, your file was not uploaded."; ?>
+            </div>
+        <?php
+        // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($image["tmp_name"], $target_file)) {
+            ?>
+            <div class="alert alert-success" role="alert">
+                <?php echo "The file " . htmlspecialchars(basename($image["name"])) . " has been uploaded."; ?>
+            </div>
+            <?php
+            // get full image url
+            $img_url = "http://20.254.55.178/Assets/Watches/" . basename($image["name"]);
+            return $img_url;
+        } else {
+            ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo "Sorry, your file was not uploaded."; ?>
+            </div>
+        <?php
+        }
+    }
+}
 
 
 ?>
