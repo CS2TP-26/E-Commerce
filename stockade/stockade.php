fe449a0768a1c59d304ee6baedfed398d7189019
Drkongy <80266694+Drkongy@users.noreply.github.com>
2022-11-29 14:47:04 +0000

    test

--- stockade/stockade.php
+++ stockade/stockade.php
@@ -254,7 +254,9 @@
 
                                                 <tr>
                                                     <th>Image:</th>
-                                                    <td><img class= "edit-img" src = "<?php echo $row['image']; ?>" alt = "..."/></img><td>
+                                                    <td><?php echo $row['image']; ?></td>
+
+                                                    <!-- <td><img class= "edit-img" src = "<?php echo $row['image']; ?>" alt = "..."/></img><td> -->
                                                     <td><i class='bx bx-lock icon'></i></td>
                                                 </tr>
 
