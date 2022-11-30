2e59b4d875e9ae288f30f534d04abdbc569af3dc
Jacques22FC <JacWinmillFC@gmail.com>
2022-11-30 21:36:28 +0000

    Basket css

--- site/basket.php
+++ site/basket.php
@@ -1,3 +1,28 @@
+<?php
+session_start();
+
+$status = "";
+$id = $_POST['id'];
+
+if (isset($_POST['action']) && $_POST['action'] == "remove") {
+	$id = $_POST['id'];
+	if (!empty($_SESSION["basket"])) {
+		foreach ($_SESSION["basket"] as $key => $value) {
+			if ($_SESSION["basket"][$key]['id'] == $id) {
+				unset($_SESSION["basket"][$key]);
+				$status = "<div class='box' style
+				='color:red;'>
+				Product is removed from your basket!</div>";
+			}
+			if (empty($_SESSION["basket"]))
+				unset($_SESSION["basket"]);
+		}
+	}
+}
+?>
+
+
+
 <!doctype html>
 <html>
 
@@ -33,7 +58,91 @@
 
 
     <div class="panel">
-        
+		<div class="basket">
+			<?php
+
+				if (isset($_SESSION["basket"])) {
+				$total_price = 0;
+			?>
+				<table>
+					<thead>
+						<tr>
+						<th>Image</th>
+						<th>Name </th>
+						<th>Price (£) </th>
+						<th>Total</th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php
+					foreach ($_SESSION["basket"] as $product) {
+					?>
+						<td>
+							<img src='<?php echo $product["image"]; ?>' width="50" height="40" />
+						</td>
+						<td><?php echo $product["name"]; ?><br />
+							<form method='post' action=''>
+								<input type='hidden' name='id' value="<?php echo $product["id"]; ?>" />
+								<input type='hidden' name='action' value="remove" />
+								<button type='submit' class='remove'>Remove Item</button>
+							</form>
+						</td>
+						<td>
+							<form method='post' action=''>
+								<input type='hidden' name='id' value="<?php echo $product["id"]; ?>" />
+								<input type='hidden' name='action' value="change" />
+							</form>
+						</td>
+						<td><?php echo "£" . $product["price"]; ?></td>
+						<td><?php echo "£" . $product["price"] * $product["quantity"]; ?></td>
+						</tr>
+					<?php
+						$total_price += ($product["price"] * $product["quantity"]);
+					}
+					?>
+					<tr>
+						<td colspan="5" align="right">
+							<strong>TOTAL: <?php echo "£" . $total_price; ?></strong>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+			<?php
+		} else {
+		echo "<h3>Your basket is empty!</h3>";
+		}
+			?>
+        </div>
+		<div style="clear:both;"></div>
+
+			<div class="message_box" style="margin:10px 0px;">
+			</div>
+
+
+			<h3><?php echo $status; ?></h3>
+
+
+			<?php
+			if (isset($_SESSION["basket"])) {
+				
+			?>
+				<!-- button to go to checkout page -->
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<button class="btn btn-primary" type="button"><a href="checkout.php">Checkout </a></button>
+						</div>
+					</div>
+				</div>
+
+			<?php
+			}
+
+
+			?>
+
+
+
     </div>
 
 
