82ad081fa35e69a7fa5600d170d56f9bc4a384df
Jacques22FC <JacWinmillFC@gmail.com>
2022-11-30 10:19:50 +0000

    Checkout fields modified

--- pages/checkout.php
+++ pages/checkout.php
@@ -48,10 +48,23 @@ if (isset($_POST['checkout'])) {
 
 
 <!DOCTYPE html>
-<html>
+
 <head>
-  <title>Checkout</title>
+    <meta charset="UTF-8">
+    <title>Checkout</title>
+    <!---Fontawesome CDN Link-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <!----Custom CSS Filw Link--->
+    <link rel="stylesheet" href="../css/navigation.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/login.css">
+
+    <!-- mobile view -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Website Icon -->
+    <!-- <link rel="icon" href="../assets/images/Icon8.png"> -->
 </head>
+
 <body>
   <h2>Contents of basket</h2>
   <div class="basket">
@@ -111,12 +124,27 @@ if (isset($_POST['checkout'])) {
   <form method = "post" action="checkout.php">
   <div>Details</div>
     
+    <label>First name</label>
+    <input id = "textbox" type="Fname" name="First name" required><br><br>
+    <label>Last name</label>
+    <input id = "textbox" type="Lname" name="Last name" required><br><br>
     <label>Name on card</label>
-    <input id = "textbox" type="name" name="name" required><br><br>
+    <input id = "textbox" type="Cname" name="name" required><br><br>
     <label>Card number</label>
     <input id = "textbox" type="card" name="card number" required><br><br>
     <label>Security number</label>
-    <input id = "textbox" type="text" name="Security number" required><br><br>
+    <input id = "textbox" type="secnum" name="Security number" required><br><br>
+    <label>Address Line 1</label>
+    <input id = "textbox" type="Address" name="Address Line 1" required><br><br>
+    <label>Address Line 2</label>
+    <input id = "textbox" type="Address2" name="Address Line 2" required><br><br>
+    <label>City</label>
+    <input id = "textbox" type="City" name="City" required><br><br>
+    <label>Postcode</label>
+    <input id = "textbox" type="Pcode" name="Postcode" required><br><br>
+
+
+
     <!-- submit button -->
     <button type="submit" name="checkout">Checkout</button>
 
@@ -125,4 +153,18 @@ if (isset($_POST['checkout'])) {
   <!-- <p> Checkout<a href="my_orders.php">Checkout</a>  </p> -->
 
 </body>
+
+<footer class="footer">
+    <div class="social">
+        <a href="https://github.com/CS2TP-26/E-Commerce" target="_blank"><i class="fab fa-github"></i></a>
+        <!-- Remake the footer -->
+    </div>
+
+    <p class="copyright">
+        &copy; Copyright 2022 Maison De Verce
+    </p>
+
+</footer>
+
+
 </html>
