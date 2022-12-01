08068e82ef11e18568e3ecb461b390c1aa6026bd
Drkongy <80266694+Drkongy@users.noreply.github.com>
2022-12-01 18:52:14 +0000

    123

--- site/about.php
+++ site/about.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <head>
-	<link rel='stylesheet' type='text/css' href='../css/styles_about.css' />
+	<link rel='stylesheet' type='text/css' href='css/styles_about.css' />
 	<link href='//fonts.googleapis.com/css?family=Montserrat:thin,extra-light,light,100,200,300,400,500,600,700,800' rel='stylesheet' type='text/css'>
 	 <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 	<meta charset="utf-8">
